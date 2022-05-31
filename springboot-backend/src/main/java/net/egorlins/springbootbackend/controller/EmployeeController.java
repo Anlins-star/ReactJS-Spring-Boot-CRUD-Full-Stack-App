bdc7a7815ecc09dd749bf05303f9cd6e258a9fc6
Jegor <egorlins@hotmail.com>
2022-05-31 10:11:30 +0300

    Initial commit. Localhost is UP.

---
+++ springboot-backend/src/main/java/net/egorlins/springbootbackend/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package net.egorlins.springbootbackend.controller;
+
+import net.egorlins.springbootbackend.model.Employee;
+import net.egorlins.springbootbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees()
+    {
+        return employeeRepository.findAll();
+    }
+}
