904d30080b73514bee4fc23e4621798a6ffd22ad
Jegor <egorlins@hotmail.com>
2022-09-19 09:09:27 +0300

    added comment

--- springboot-backend/src/main/java/net/egorlins/springbootbackend/exception/ResourceNotFoundException.java
+++ springboot-backend/src/main/java/net/egorlins/springbootbackend/exception/ResourceNotFoundException.java
@@ -3,6 +3,7 @@ package net.egorlins.springbootbackend.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+//if record not exist in database, then REST API will return HttpStatus.NOT_FOUND to the client
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
     private static final long serialVersionUID = 1L;
