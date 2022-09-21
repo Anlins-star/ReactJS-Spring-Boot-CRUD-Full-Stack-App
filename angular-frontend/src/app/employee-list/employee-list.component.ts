7f0206e1aa7cadece3628d74c9e71693b15314fa
Jegor <egorlins@hotmail.com>
2022-09-21 19:01:58 +0300

    populating employee list page with data provided from springboot

--- angular-frontend/src/app/employee-list/employee-list.component.ts
+++ angular-frontend/src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -8,41 +9,17 @@ import { Employee } from '../employee';
 })
 export class EmployeeListComponent implements OnInit {
 
-  employees: Employee[];
+  employees: Employee[] | undefined;
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
+    this.getEmployees();
+  }
 
-    //temporary adding hardcoded employees to array
-    //copied from postman
-    this.employees = [
-      {
-          "id": 24,
-          "firstName": "1yruer22",
-          "lastName": "2ytuyeuyt",
-          "emailId": "333  3332132@ddddf.hh"
-      },
-      {
-          "id": 25,
-          "firstName": "545454",
-          "lastName": "Last",
-          "emailId": "dggd@djjdjd.ee"
-      },
-      {
-          "id": 26,
-          "firstName": "3434",
-          "lastName": "343443",
-          "emailId": "23wr@fssfdf.gg"
-      },
-      {
-          "id": 37,
-          "firstName": "23423",
-          "lastName": "42323",
-          "emailId": "fasdfa"
-      }
-  ];
-
-}
-
+  private getEmployees(){
+    this.employeeService.getEmployeesList().subscribe(data => {
+      this.employees = data;
+    });
+  }
 }
