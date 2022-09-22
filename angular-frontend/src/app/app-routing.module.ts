5d158e846208686dbfd70ee9f4f9a2e74bf4408f
Jegor <egorlins@hotmail.com>
2022-09-22 08:04:52 +0300

    added routing menu anf footer

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'emplyoees', component: EmployeeListComponent},
+  {path: '', redirectTo: 'emplyoees', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
